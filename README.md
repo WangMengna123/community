8fd51b06ca0c068603452d62b15008179ccf6f64
WangMengna123 <1874621938@qq.com>
2019-09-09 20:07:47 +0800

    authorize/modify application.properties

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 #麻将社区
 
 ##资料
-https://spring.io/guides
-
-https://spring.io/guides/gs/serving-web-content/#scratch
-
-https://elasticsearch.cn/explore
+[Spring 文档](https://spring.io/guides)
+[Spring web文档](https://spring.io/guides/gs/serving-web-content/#scratch)
+[ES社区](https://elasticsearch.cn/explore)
+[Bootstrap 文档](https://v3.bootcss.com/css/#buttons)
+[Github OAuth](https://developer.github.com/apps/building-oauth-apps/)
 
 ##工具
-https://www.git-scm.com/download/
+[Git](https://www.git-scm.com/download/)
+
+#笔记
+###登录
+1. 用户点击登录按钮后，流程如下
+    + 社区调用GitHub的授权页面
+    + GitHub授权后回调社区的redirect_uri并携带code
+    + 客户端用code来申请令牌token
+    + GitHub确认无误后发放令牌
+    + 客户端用令牌向GitHub申请用户信息
+    + GitHub确认无误返回信息
+    + 存入数据库，更新登录状态
+2. okhttp
+
---
+++ src/main/java/com/program/community/controller/AuthorizeController.java
@@ -0,0 +1,37 @@
+package com.program.community.controller;
+
+import com.program.community.dto.AccessTokenDto;
+import com.program.community.dto.GitHubUser;
+import com.program.community.provider.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GitHubProvider gitHubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.client.redirect_uri}")
+    private String redirectUri;
+    @GetMapping("/callback")
+    public String callBack(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
+        accessTokenDto.setCode(code);
+        accessTokenDto.setRedirect_uri(redirectUri);
+        accessTokenDto.setState(state);
+        String  accessToken = gitHubProvider.getAccessToken(accessTokenDto);
+        GitHubUser user = gitHubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
